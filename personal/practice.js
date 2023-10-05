029cca9537f4d6c82935f20611a13bcdc794d0e4
BriiProgrammer <145430813+BriiProgrammer@users.noreply.github.com>
2023-10-05 20:47:04 +0800

    Delete personal directory

--- personal/practice.js
+++
@@ -1,5 +0,0 @@
-let text = document.querySelector(".text p");
-
-  text.innerHTML = text.innerHTML.split("").map((char,i)=>
-  `<b style="transform:rotate(${i * 77.7}deg")>${char}</b>`
-  ).join("");
